4801c26e3cd05baa02de7550cdc919ad3b349120
m3b1u5 <dbatezat@gmail.com>
2024-12-02 11:57:54 +0200

    Revised

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 
     print(my_list)
 
-    my_list[0], my_list[len(my_list) - 1] = my_list[len(my_list) - 1], my_list[0]
+    my_list[0], my_list[-1] = my_list[-1], my_list[0]
 
     print(my_list)
 
